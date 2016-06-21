544fd034a91f24ee403a8d6f320d435ad1d83d17
skipperguy12 <neiljohari@gmail.com>
2016-06-21 13:19:49 -0400

    Add multiple screens, copy basic texturing and fonts for ease

---
+++ core/src/main/java/rocks/astroid/astroid/core/screens/MainMenu.java
@@ -0,0 +1,196 @@
+package rocks.astroid.astroid.core.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.run;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenManager;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import rocks.astroid.astroid.core.Astroid;
+
+public class MainMenu implements Screen {
+
+    private Stage stage;
+
+    private Skin skin;
+    private Table table;
+    private TextButton buttonPlay, buttonExit;
+    private TweenManager tweenManager;
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+
+        tweenManager.update(delta);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+        //stage.getViewport().update(width, height);
+        table.invalidateHierarchy();
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.log("astroid.rocks", "Displaying MainMenu");
+
+        stage = new Stage();
+
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("ui/menuSkin.json"), new TextureAtlas(Gdx.files.internal("ui/atlas.pack")));
+
+        table = new Table(skin);
+        table.setFillParent(true);
+
+        // creating heading
+        //Label heading = new Label(Astroid.TITLE, skin, "big");
+        //heading.setFontScale(2);
+
+        // creating buttons
+        TextButton buttonPlay = new TextButton("PLAY", skin, "big");
+        buttonPlay.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                stage.addAction(sequence(moveTo(0, -stage.getHeight(), .5f), run(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Gdx.app.log("astroid.rocks", "Changing from MainMenu to Play");
+                        ((Game) Gdx.app.getApplicationListener()).setScreen(new Play());
+                    }
+                })));
+            }
+        });
+        buttonPlay.pad(15);
+
+        TextButton buttonSettings = new TextButton("SETTINGS", skin);
+        buttonSettings.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                stage.addAction(sequence(moveTo(0, -stage.getHeight(), .5f), run(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            //throw new Exception("Not yet implemented");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        //((Game) Gdx.app.getApplicationListener()).setScreen(new Settings());
+                    }
+                })));
+            }
+        });
+        buttonSettings.pad(15);
+
+        TextButton buttonExit = new TextButton("EXIT", skin, "big");
+        buttonExit.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Timeline.createParallel().beginParallel()
+                        .push(Tween.to(table, ActorAccessor.ALPHA, .75f).target(0))
+                        .push(Tween.to(table, ActorAccessor.Y, .75f).target(table.getY() - 50)
+                                .setCallback(new TweenCallback() {
+
+                                    @Override
+                                    public void onEvent(int type, BaseTween<?> source) {
+                                        Gdx.app.exit();
+                                    }
+                                }))
+                        .end().start(tweenManager);
+            }
+        });
+        buttonExit.pad(15);
+
+        // putting stuff together
+        //table.add(heading).spaceBottom(100).row();
+        table.add(buttonPlay).spaceBottom(15).row();
+        table.add(buttonSettings).spaceBottom(15).row();
+        table.add(buttonExit);
+
+        stage.addActor(table);
+
+        // creating animations
+        tweenManager = new TweenManager();
+        Tween.registerAccessor(Actor.class, new ActorAccessor());
+
+        /*
+        // heading color animation
+        Timeline.createSequence().beginSequence()
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 0, 1))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 1, 0))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 0, 0))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 1, 0))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 1, 1))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 0, 1))
+                .push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 1, 1))
+                .end().repeat(Tween.INFINITY, 0).start(tweenManager);
+        */
+        // heading and buttons fade-in
+        Timeline.createSequence().beginSequence()
+                .push(Tween.set(buttonPlay, ActorAccessor.ALPHA).target(0))
+                .push(Tween.set(buttonSettings, ActorAccessor.ALPHA).target(0))
+                .push(Tween.set(buttonExit, ActorAccessor.ALPHA).target(0))
+                //.push(Tween.from(heading, ActorAccessor.ALPHA, .25f).target(0))
+                .push(Tween.to(buttonPlay, ActorAccessor.ALPHA, .25f).target(1))
+                .push(Tween.to(buttonSettings, ActorAccessor.ALPHA, .25f).target(1))
+                .push(Tween.to(buttonExit, ActorAccessor.ALPHA, .25f).target(1))
+                .end().start(tweenManager);
+
+
+        // table fade-in
+        Tween.from(table, ActorAccessor.ALPHA, .75f).target(0).start(tweenManager);
+        Tween.from(table, ActorAccessor.Y, .75f).target(Gdx.graphics.getHeight() / 8).start(tweenManager);
+
+        tweenManager.update(Gdx.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void hide() {
+        Gdx.app.log("astroid.rocks", "Hiding MainMenu");
+    }
+
+    @Override
+    public void pause() {
+
+        Gdx.app.log("astroid.rocks", "Pausing MainMenu");
+    }
+
+    @Override
+    public void resume() {
+
+        Gdx.app.log("astroid.rocks", "Resuming MainMenu");
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/main/java/rocks/astroid/astroid/core/screens/Splash.java
+++ core/src/main/java/rocks/astroid/astroid/core/screens/Splash.java
@@ -1,5 +1,10 @@
 package rocks.astroid.astroid.core.screens;
 
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenManager;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,11 +12,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import rocks.astroid.astroid.core.tween.SpriteAccessor;
 
 public class Splash implements Screen {
     private SpriteBatch batch;
     private Sprite splash;
     private Sprite rocket;
+    private TweenManager tweenManager;
     private float timer;
 
     @Override
@@ -20,18 +27,21 @@ public class Splash implements Screen {
         Gdx.gl.glClearColor(0,0,0,1); // sets color
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clear
 
+        tweenManager.update(delta);
+
+        /*
         float xOffset = (Gdx.graphics.getWidth() / 2) - (rocket.getX() + (rocket.getWidth() / 2));
         float yOffset = (Gdx.graphics.getHeight() / 2) - (rocket.getY() + (rocket.getHeight() / 2));
         float mouseX = Gdx.input.getX() + xOffset;
         float mouseY = (Gdx.graphics.getHeight() - Gdx.input.getY()) + yOffset;
 
-        float radiansToMouse = (float) Math.atan2(mouseX - (Gdx.graphics.getWidth() / 2), (Gdx.graphics.getHeight() / 2) - mouseY);
+        float radiansToMouse = MathUtils.atan2(mouseX - (Gdx.graphics.getWidth() / 2), (Gdx.graphics.getHeight() / 2) - mouseY);
         float degreesToMouse = MathUtils.radiansToDegrees * radiansToMouse;
         rocket.setRotation(degreesToMouse - 90);
 
-        rocket.setX((float) (rocket.getX() + Math.cos(degreesToMouse - 90) * 1));
-        rocket.setY((float) (rocket.getY() + Math.sin(degreesToMouse - 90) * 1));
-
+        rocket.setX((float) (rocket.getX() + MathUtils.cos(degreesToMouse - 90) * 1));
+        rocket.setY((float) (rocket.getY() + MathUtils.sin(degreesToMouse - 90) * 1));
+        */
 
         batch.begin();
         splash.draw(batch);
@@ -47,6 +57,9 @@ public class Splash implements Screen {
     @Override
     public void show() {
         batch = new SpriteBatch();
+        tweenManager = new TweenManager();
+        Tween.registerAccessor(Sprite.class, new SpriteAccessor());
+
 
         splash = new Sprite(new Texture("img/splash.png"));
         rocket = new Sprite(new Texture("img/sprites/ships/fighter/ship_blue.png"));
@@ -54,8 +67,24 @@ public class Splash implements Screen {
 
         rocket.setCenter(Gdx.graphics.getWidth()/2, (Gdx.graphics.getHeight()/2));
         rocket.setScale(.75f, .75f);
-        rocket.rotate90(true);
-        System.out.println(rocket.getX() + " "  + rocket.getY());
+        //rocket.rotate90(true);
+        //System.out.println(rocket.getX() + " "  + rocket.getY());
+
+        Tween.set(splash, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+        Tween.to(splash, SpriteAccessor.ALPHA, 2).target(1).repeatYoyo(1, 2).start(tweenManager);
+
+        Tween.set(rocket, SpriteAccessor.ALPHA).target(0).start(tweenManager);
+        Tween.to(rocket, SpriteAccessor.ALPHA, 2).target(1).repeatYoyo(1, 2).start(tweenManager).setCallback(new TweenCallback() {
+            @Override
+            public void onEvent(int type, BaseTween<?> source) {
+                Gdx.app.log("astroid.rocks", "Changing from Splash screen to Main menu");
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+            }
+        });
+
+        Tween.set(rocket, SpriteAccessor.SCALE).target(0, 0).start(tweenManager);
+        Tween.to(rocket, SpriteAccessor.SCALE, 1).target(.75f, .75f).start(tweenManager);
+
     }
 
     @Override
@@ -77,5 +106,6 @@ public class Splash implements Screen {
     public void dispose() {
         batch.dispose();
         splash.getTexture().dispose();
+        rocket.getTexture().dispose();
     }
 }
