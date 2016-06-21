32d4107c1a82a166f4a1f62b443063f34e35d6d4
Matt Raneri <mattraneri@gmail.com>
2016-06-20 22:20:09 -0400

    Added mass field and changed speed to thrust

--- core/src/main/java/rocks/astroid/astroid/core/logic/Ship.java
+++ core/src/main/java/rocks/astroid/astroid/core/logic/Ship.java
@@ -9,7 +9,9 @@ public abstract class Ship {
     //Ship Location vector will contain: x, y, and rotation in degrees.
     private Vector3 shipLocation;
     //Speed of ship- defaults to 1
-    private float speed;
+    private float thrust;
+    //Mass of the ship- used in combination with thrust to determine acceleration
+    private float mass;
 
 
     /**
@@ -17,43 +19,54 @@ public abstract class Ship {
      */
     public Ship() {
         shipLocation = new Vector3(0f, 0f, 0f);
-        speed = 1;
+        thrust = 1;
+        mass = 1;
     }
 
     /**
-     * X, Y, Rotation values set- speed set to default (1)
+     * X, Y, Rotation values set- thrust set to default (1)
      */
     public Ship(float x, float y, float rotation) {
         shipLocation = new Vector3(x, y, rotation);
-        speed = 1;
+        thrust = 1;
+        mass = 1;
     }
 
     /**
-     * This is the typical use constructor- ship location and speed
+     * This is the typical use constructor- ship location, thrust and mass
      */
-    public Ship(float x, float y, float rotation, float speed) {
+    public Ship(float x, float y, float rotation, float thrust, float mass) {
         shipLocation = new Vector3(x, y, rotation);
-        this.speed = speed;
+        this.thrust = thrust;
+        this.mass = mass;
     }
 
     /**
      * Takes ship location as a vector3
      */
-    public Ship(Vector3 shipLocation, float speed) {
+    public Ship(Vector3 shipLocation, float thrust) {
         this.shipLocation = shipLocation;
-        this.speed = speed;
+        this.thrust = thrust;
     }
 
     public abstract void draw();
 
     public abstract void update();
 
-    public float getSpeed() {
-        return speed;
+    public float getThrust() {
+        return thrust;
     }
 
-    public void setSpeed(float speed) {
-        this.speed = speed;
+    public void setThrust(float thrust) {
+        this.thrust = thrust;
+    }
+
+    public float getMass() {
+        return mass;
+    }
+
+    public void setMass(float mass) {
+        this.mass = mass;
     }
 
     /**
