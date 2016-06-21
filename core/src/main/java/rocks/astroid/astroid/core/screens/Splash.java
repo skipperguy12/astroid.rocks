2a1bc565a1fb2f0f16db766a5895eb486529e7f6
skipperguy12 <neiljohari@gmail.com>
2016-06-20 22:47:23 -0400

    Functioning moving ship

--- core/src/main/java/rocks/astroid/astroid/core/screens/Splash.java
+++ core/src/main/java/rocks/astroid/astroid/core/screens/Splash.java
@@ -6,16 +6,32 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Splash implements Screen {
     private SpriteBatch batch;
     private Sprite splash;
     private Sprite rocket;
+    private float timer;
 
     @Override
     public void render(float delta) {
+        timer += delta;
         Gdx.gl.glClearColor(0,0,0,1); // sets color
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clear
+
+        float xOffset = (Gdx.graphics.getWidth() / 2) - (rocket.getX() + (rocket.getWidth() / 2));
+        float yOffset = (Gdx.graphics.getHeight() / 2) - (rocket.getY() + (rocket.getHeight() / 2));
+        float mouseX = Gdx.input.getX() + xOffset;
+        float mouseY = (Gdx.graphics.getHeight() - Gdx.input.getY()) + yOffset;
+
+        float radiansToMouse = (float) Math.atan2(mouseX - (Gdx.graphics.getWidth() / 2), (Gdx.graphics.getHeight() / 2) - mouseY);
+        float degreesToMouse = MathUtils.radiansToDegrees * radiansToMouse;
+        rocket.setRotation(degreesToMouse - 90);
+
+        rocket.setX((float) (rocket.getX() + Math.cos(degreesToMouse - 90) * 1));
+        rocket.setY((float) (rocket.getY() + Math.sin(degreesToMouse - 90) * 1));
+
 
         batch.begin();
         splash.draw(batch);
@@ -35,8 +51,11 @@ public class Splash implements Screen {
         splash = new Sprite(new Texture("img/splash.png"));
         rocket = new Sprite(new Texture("img/sprites/ships/fighter/ship_blue.png"));
         splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        rocket.setCenter(Gdx.graphics.getWidth()/2, (Gdx.graphics.getHeight()/2));
         rocket.setScale(.75f, .75f);
-        rocket.setCenter(Gdx.graphics.getWidth()/2, (Gdx.graphics.getHeight()/2) - 50);
+        rocket.rotate90(true);
+        System.out.println(rocket.getX() + " "  + rocket.getY());
     }
 
     @Override
--- desktop/src/main/java/rocks/astroid/astroid/java/AstroidDesktop.java
+++ desktop/src/main/java/rocks/astroid/astroid/java/AstroidDesktop.java
@@ -9,7 +9,7 @@ public class AstroidDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.vSyncEnabled = true;
-		config.useGL30 = true;
+		//config.useGL30 = true;
 		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new Astroid(), config);
